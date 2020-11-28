ea0e49cdca613cb0f5662b169be20bdc12ee01ab
Alexandre Carbenay <alexandre.carbenay@zenika.com>
2020-11-28 22:46:02 +0100

    Initial commit

---
+++ src/main/java/com/zenika/ddd/LabsDddApplication.java
@@ -0,0 +1,13 @@
+package com.zenika.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zenika/ddd/LabsDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
